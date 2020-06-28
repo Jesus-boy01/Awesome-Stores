f4e064e7b9cc93380a8b950cdde978c6e37a5d0d
Tolulope Dada <tdada0912@gmail.com>
2020-06-28 10:43:05 +0100

    my first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+	
+	   /* Animations on scroll  */ 
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+});
+
+
+
